3d039d8e447867ac6a365cfb0456fce102bcc391
Amit Sharma <amitsharmar249@gmail.com>
2026-09-01 02:20:16 +0530

    add cloud db

--- app/database.py
+++ app/database.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DB_HOST: str
+    DB_PORT: int
     DB_USER: str
     DB_PASSWORD: str
     DB_NAME: str
@@ -14,24 +15,35 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
-
-
 def get_db_connection():
+
     conn = pymysql.connect(
         host=settings.DB_HOST,
+        port=settings.DB_PORT,
         user=settings.DB_USER,
         password=settings.DB_PASSWORD,
         database=settings.DB_NAME,
-        cursorclass=pymysql.cursors.DictCursor
+        cursorclass=pymysql.cursors.DictCursor,
+        charset="utf8mb4",
+        connect_timeout=10
     )
+
     return conn
 
 
-if __name__ == "__main__":
-    try:
-        conn = get_db_connection()
-        if conn:
-            print("MYSQL connection is successful")
-        conn.close()
-    except Exception as e:
-        print(f"Error: {e}")
+
+# if __name__ == "__main__":
+#     try:
+#         conn = get_db_connection()
+#         if conn:
+#             print("MYSQL connection is successful")
+#         cursor = conn.cursor()
+#         cursor.execute("SELECT VERSION() AS version")
+
+#         print(cursor.fetchone())
+
+#         cursor.close()
+#         conn.close()
+
+#     except Exception as e:
+#         print(f"Error: {e}")
